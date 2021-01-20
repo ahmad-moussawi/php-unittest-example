4eb22ea78cd473d49a5f7300821747dbc3274ca0
Ahmad Moussawi <aymoussawi@gmail.com>
2021-01-20 14:05:34 +0200

    add collision output

--- src/EntryPrinter.php
+++ src/EntryPrinter.php
@@ -5,7 +5,7 @@ namespace App;
 class EntryPrinter
 {
     function print(array $entries) {
-        $mask = "|%-10s |%10s\n";
+        $mask = "|%-10s |%10s" . PHP_EOL;
         printf("\n");
         foreach ($entries as $entry) {
             printf($mask, $entry['account'], $entry['amount']);
